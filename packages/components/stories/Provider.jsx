09ab01fb511fbf30b349a5284cd61b05308ec007
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-12-09 18:33:27 -0200

    Added storybook addons

--- packages/components/stories/Provider.jsx
+++ packages/components/stories/Provider.jsx
@@ -1,12 +1,12 @@
 import React from "react"
 import { storiesOf } from "@storybook/react"
-import { action } from "@storybook/addon-actions"
-import { linkTo } from "@storybook/addon-links"
+import { action } from "@storybook/addon-info"
 import { PrismicProvider } from "../src"
 
-storiesOf("Provider", module)
+storiesOf("components/Provider", module)
   .add("basic usage", () => (
     <PrismicProvider>
       Hello World
     </PrismicProvider>
   ))
+
