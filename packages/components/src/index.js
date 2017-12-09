c120ca3e0fca4c96cc1cd9b11c00f2a79748346a
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-12-09 17:19:36 -0200

    Fixed components package main script

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -3,3 +3,8 @@ export { default as Image } from "./Image"
 export { default as RichText } from "./RichText"
 // export { default as Embed } from "./Embed"
 export { default as SliceZone } from "./SliceZone"
+export {
+  default as Provider,
+  prismicShape,
+  withPrismicContext
+} from "./Provider"
