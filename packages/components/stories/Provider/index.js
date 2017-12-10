854a990f9de95583dc96ad6c422db083929108db
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-12-10 20:56:46 -0200

    Removed storybook

--- packages/components/stories/Provider/index.js
+++
@@ -1,5 +0,0 @@
-import { storiesOf } from "@storybook/react"
-
-storiesOf("Provider", module)
-  .add("basic usage", require("./basic-usage").default)
-  .add("react-router", require("./react-router").default)
--- packages/redux/stories/Document/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import { storiesOf } from "@storybook/react"
-import { Provider } from "react-redux"
-import createStore from "../store"
-
-storiesOf("Document", module)
-  .addDecorator(story => <Provider store={createStore()}>{story()}</Provider>)
-  .add("basic usage", require("./basic-usage").default)
