086c8724364a7ded31cf88fc87efed433672fd79
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-12-11 07:20:33 -0200

    Fixed selectors

--- packages/redux/src/Feed/selectors.js
+++ packages/redux/src/Feed/selectors.js
@@ -14,13 +14,13 @@ export const getPagination = createSelector(getFeed, feed => feed.pagination || 
 export const getPageData = createSelector(
   getFeed,
   (state, { page }) => page,
-  (pages, page) => pages[page] || {}
+  ({ pages }, page) => (pages && pages[page]) || {}
 )
 
 export const getPageDocuments = createSelector(
   getDocType,
   getPageData,
-  (documents, page) => (page.docs ? page.docs.map(uid => documents[uid]) : undefined)
+  (documents, page) => (page.docs ? page.docs.map(uid => documents[uid].doc) : undefined)
 )
 
 export const getPageError = createSelector(getPageData, page => page.error)
