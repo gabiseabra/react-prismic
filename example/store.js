f354bf7d572cc24883b3eda9aa8ea4be0ccc26f8
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-12-11 05:59:55 -0200

    Added redux logger

--- example/store.js
+++ example/store.js
@@ -1,12 +1,14 @@
 import { createStore, combineReducers, applyMiddleware } from "redux"
 import createSagaMiddleware from "redux-saga"
+import logger from "redux-logger"
 import prismicSaga, { PrismicClient } from "../packages/saga/src"
 import { prismicReducer } from "../packages/redux/src"
 
 export default function create() {
   const sagaMiddleware = createSagaMiddleware()
   const reducer = combineReducers({ prismic: prismicReducer })
-  const store = createStore(reducer, applyMiddleware(sagaMiddleware))
+  const enhancers = applyMiddleware(sagaMiddleware, logger)
+  const store = createStore(reducer, enhancers)
   const apiClient = new PrismicClient("example-repo")
   const saga = prismicSaga(apiClient)
   sagaMiddleware.run(saga)
