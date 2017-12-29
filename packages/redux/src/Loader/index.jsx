99a985e2811d5675e3125b22a1bc191b00ff2c43
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-12-28 23:22:06 -0200

    Fixed lodash.pick args order

--- packages/redux/src/Loader/index.jsx
+++ packages/redux/src/Loader/index.jsx
@@ -34,20 +34,21 @@ class PrismicLoader extends React.Component {
   componentWillReceiveProps(next) {
     const { load, data, loading } = this.props
     if(data || loading) return
-    const options = pick(optionsProps, next)
+    const options = pick(next, optionsProps)
     const sameOptions = eql(this.options, options)
     const sameDocument = (next.type === this.props.type && next.uid === this.props.uid)
     if(sameOptions || !sameDocument) return
     load(options)
   }
 
-  get options() { return pick(optionsProps, this.props) }
+  get options() { return pick(this.props, optionsProps) }
 
-  get childProps() { return pick(childProps, this.props) }
+  get childProps() { return pick(this.props, childProps) }
 
   render() {
     const { children, component: Component } = this.props
     const props = this.childProps
+    console.log(props)
     if(Component) {
       return <Component {...props} />
     }
